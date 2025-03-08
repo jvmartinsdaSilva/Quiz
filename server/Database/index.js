9aace52a8ae108f3758fac1775ed26780a7cde07
jvmartinsdaSilva <jvmartins0855@gmail.com>
2025-03-08 14:23:46 -0300

    Add Pool connections in database and handling connection errors

--- server/Database/index.js
+++ server/Database/index.js
@@ -1,24 +1,36 @@
 import pg from 'pg'
-const {Client} = pg
 
-const client = new Client({
+const { Pool } = pg
+const pool = new Pool({
     user: 'joao',
     password: 'example',
     host: 'database',
     port: 5432,
-    database: 'quizapp'
+    database: 'quizapp',
+    max: 30
 })
 
-await client.connect()
 
 export const getAnyQuestions = async questionTheme => {
-    const query = await client.query(`SELECT * FROM question WHERE question.theme = '${questionTheme}' ORDER BY RANDOM() LIMIT 3 `).catch(err => {return {erro: err}})
-    const questions = query.rows
-    return questions
+    const query = `SELECT * FROM question WHERE question.theme = '${questionTheme}' ORDER BY RANDOM() LIMIT 3`
+    try {
+        const client = await pool.connect()
+        const datas = await client.query(query)
+        client.release(true)
+        return {datas: datas.rows}
+    } catch (err) {
+        return { message: "Erro ao se conetar ao banco", erro: err }
+    }
 }
 
 export const getThemes = async () => {
-    const query = await client.query("SELECT DISTINCT theme FROM question ").catch(err => {return {erro: err}})
-    const themes = query.rows?.map(theme => theme.theme)
-    return themes
+    const query = "SELECT DISTINCT theme FROM question"
+    try{
+        const client = await pool.connect()
+        const datas = await client.query(query)
+        client.release(true)       
+        return {themes: datas.rows.map(data => data.theme)}
+    }  catch (err) {
+        return { message: "Erro ao se conetar ao banco", erro: err }
+    }
 }
--- server/Routers/Questions/index.js
+++ server/Routers/Questions/index.js
@@ -4,17 +4,17 @@ import { getAnyQuestions, getThemes } from '../../Database/index.js'
 export const QuestionRouter = express.Router()
 
 QuestionRouter.get("/getQuestions", async (req, res) => {
-    const themes = await getThemes()
+    const {themes} = await getThemes()
     const questionTheme = req.body.questionTheme
     if(themes.indexOf(questionTheme) < 0) return res.status(422).send(JSON.stringify({message: "Valores invalidos"}))
+
     const questions = await getAnyQuestions(questionTheme)
-    if(questions?.erro) return res.status(502).send(JSON.stringify({message: "Erro ao conectar ao banco " + questions.erro}))
+    if(questions.erro) return res.status(500).send(JSON.stringify({message: questions.message}))
     return res.status(202).send(JSON.stringify(questions))
 })
 
 QuestionRouter.get("/themes", async (req, res) => {
     const themes = await getThemes()
-    if(themes?.erro) return res.status(502).send(JSON.stringify({message: "Erro ao conectar ao banco " + get_themes.erro}))
-    
-    return res.status(202).send(JSON.stringify({themes}))
+    if(themes?.erro) return res.status(502).send(JSON.stringify({message: "Erro ao conectar ao banco " + themes.erro}))
+    return res.status(202).send(JSON.stringify(themes))
 })
