9fe682766996f30de8a5b9df48efa2375fa21785
jvmartinsdaSilva <jvmartins0855@gmail.com>
2025-03-07 23:26:07 -0300

    Reconfig router question to get questions the themes

--- server/Database/index.js
+++ server/Database/index.js
@@ -11,12 +11,14 @@ const client = new Client({
 
 await client.connect()
 
-export const getAnyQuestions = async amount => {
-    const questions = await client.query(`SELECT * FROM question ORDER BY RANDOM() LIMIT ${amount}`).catch(err => {return {erro: err}})
-    return questions.rows
+export const getAnyQuestions = async questionTheme => {
+    const query = await client.query(`SELECT * FROM question WHERE question.theme = '${questionTheme}' ORDER BY RANDOM() LIMIT 3 `).catch(err => {return {erro: err}})
+    const questions = query.rows
+    return questions
 }
 
 export const getThemes = async () => {
-    const themes = await client.query("SELECT DISTINCT theme FROM question ").catch(err => {return {erro: err}})
-    return themes.rows
+    const query = await client.query("SELECT DISTINCT theme FROM question ").catch(err => {return {erro: err}})
+    const themes = query.rows?.map(theme => theme.theme)
+    return themes
 }
--- server/Routers/Questions/index.js
+++ server/Routers/Questions/index.js
@@ -4,17 +4,17 @@ import { getAnyQuestions, getThemes } from '../../Database/index.js'
 export const QuestionRouter = express.Router()
 
 QuestionRouter.get("/getQuestions", async (req, res) => {
-    const questionsAmount = Number(req.body.questionsAmount)
-    if(questionsAmount > 5  || !questionsAmount) return res.status(422).send(JSON.stringify({message: "Valores invalidos"}))
-    const questions = await getAnyQuestions(questionsAmount)
+    const themes = await getThemes()
+    const questionTheme = req.body.questionTheme
+    if(themes.indexOf(questionTheme) < 0) return res.status(422).send(JSON.stringify({message: "Valores invalidos"}))
+    const questions = await getAnyQuestions(questionTheme)
     if(questions?.erro) return res.status(502).send(JSON.stringify({message: "Erro ao conectar ao banco " + questions.erro}))
     return res.status(202).send(JSON.stringify(questions))
 })
 
 QuestionRouter.get("/themes", async (req, res) => {
-    const get_themes = await getThemes()
-    if(get_themes?.erro) return res.status(502).send(JSON.stringify({message: "Erro ao conectar ao banco " + get_themes.erro}))
+    const themes = await getThemes()
+    if(themes?.erro) return res.status(502).send(JSON.stringify({message: "Erro ao conectar ao banco " + get_themes.erro}))
     
-    const themes = get_themes?.map(theme => theme.theme)   
     return res.status(202).send(JSON.stringify({themes}))
 })
