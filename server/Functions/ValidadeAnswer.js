eafdcf6fa362d182780853ce6be94ec2afd14944
jvmartinsdaSilva <jvmartins0855@gmail.com>
2025-04-16 22:24:11 -0300

    Add modificate for return title question in validate answer

--- server/Functions/ValidadeAnswer.js
+++ server/Functions/ValidadeAnswer.js
@@ -1,11 +1,12 @@
 export const validateUserAnswer = (userAnswers, questions) => {
     // Pega apenas as resposta da questão no formato: questionId: righ_option  {'1': option_a}
-    const answerQuestions = questions.reduce((a, v) => ({ ...a, [v.id]: v.right_option}), {}) 
+    const answerQuestions = questions.reduce((a, v) => ({ ...a, [v.id]: {right_option: v.right_option, title: v.title}}), {}) 
 
     const answerValidates = userAnswers.map(userAnswer => {
         const questionsId = Object.keys(userAnswer)[0]
-        const isCorrect = answerQuestions[questionsId] == userAnswer[questionsId]
-        return {questionsId, isCorrect}
+        const {right_option, title} = answerQuestions[questionsId]
+        const isCorrect = right_option == userAnswer[questionsId]
+        return {questionsId, isCorrect, title}
     })
     return answerValidates
 }
