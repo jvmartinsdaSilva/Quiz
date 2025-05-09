2c089463a8295b3b7e47e5510573f4352cd8e1fe
jvmartinsdaSilva <jvmartins0855@gmail.com>
2025-05-09 08:20:03 -0300

    Create function a encripiting password

---
+++ server/Functions/EncryptPassword.js
@@ -0,0 +1,32 @@
+import bcrypt from 'bcrypt'
+
+class SaltGenerator{
+    generateSalt(){
+        return new Promise((resolve, reject) => {
+            bcrypt.genSalt(10, (err, salt) => {
+                if(err) return reject(err)
+                
+                return resolve(salt)
+            } )
+        }) 
+    }
+}
+
+class HashGenerator{
+    generateHash(passowrd, salt){
+        return new Promise((resolve, reject) => {
+            bcrypt.hash(passowrd, salt, (err, hash) => {
+                if(err) return reject(err)
+                
+                return resolve(hash)
+            })
+        })
+    }
+}
+
+export const encryptingPassword = async (password) => {
+    const salt = await new SaltGenerator().generateSalt()
+    const hash = await new HashGenerator().generateHash(password, salt)
+
+    return hash
+}
--- server/main.js
+++ server/main.js
@@ -2,10 +2,12 @@ import express from 'express'
 import cors from 'cors'
 
 import { QuestionRouter } from './Routers/Questions/index.js'
+import { UserRouter } from './Routers/User/index.js'
 
 const server = express()
 server.use(cors())
 server.use(express.json())
 
 server.use("/questions", QuestionRouter)
+server.use("/users", UserRouter)
 server.listen(3001, () => console.log("http://localhost:3001"))
