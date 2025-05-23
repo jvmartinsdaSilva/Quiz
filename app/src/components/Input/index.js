1694819095c01f02ac1639ccd194df6876632d76
jvmartinsdaSilva <jvmartins0855@gmail.com>
2025-05-23 08:04:51 -0300

    Create a messages error for Register Form

--- app/src/components/Input/index.js
+++ app/src/components/Input/index.js
@@ -1,13 +1,13 @@
 import s  from './index.module.css'
 
 
-export const Input = ({type, placeholder, text}) => {
+export const Input = ({type, placeholder, text, innerRef = ''}) => {
     const ramdomId = Math.random()
 
     return (
     <label htmlFor={ramdomId} className={s.inputContainer}>
         <span>{text}</span> 
-        <input type={type} placeholder={placeholder} id={ramdomId} />
+        <input type={type} placeholder={placeholder} id={ramdomId} {...innerRef} />
     </label>
     )
 }
--- app/src/components/forms/RegisterForm/index.js
+++ app/src/components/forms/RegisterForm/index.js
@@ -1,14 +1,43 @@
+import { useForm } from "react-hook-form";
+import { object, string, ref } from 'yup'
+
+import { yupResolver } from "@hookform/resolvers/yup"  
+
 import { Button } from "../../Button/index.js"
 import { Input } from "../../Input/index.js"
 
+import s from '../index.module.css'
+
+
+const userSchema = object({
+    name: string("Nome deve ser uma texto").min(3, "Nome deve conter mais do que 3 caracteres").required("Nome não pode ser vazio"),
+    email: string("Email deve ser um texto").email("Email inválido").required("Email não pode ser vazio"),
+    password: string("Senha deve ser uma texto").min(8, "Senha deve ter no mínimo 8 caracteres").required("Senhã não pode ser vazia"),
+    confirmPassword: string().oneOf([ref("password"), null], 'As senhas não conferem').required("Campo obrigatório"),
+})
+
+
 export const RegisterForm = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        mode: "all",
+        resolver: yupResolver(userSchema)
+    })
+
+    const handleSubmitUser = async datas => {
+        console.log(datas)
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(handleSubmitUser)}>
             <h3 className='title'>CADASTRO</h3>
-            <Input text={"Nome"} placeholder="Informe o seu Nome"/>
-            <Input text={"Email"} placeholder="Informe seu e-mail" type='email'/>
-            <Input text={"Password"} placeholder="Informe seu e-mail" type="password"/>
-            <Input text={"Confirme sua senha"} placeholder="Confirme sua senha" type="password"/>
+            <Input text={"Nome"} placeholder="Informe o seu Nome" innerRef={register("name")}/>
+            <span className={s.messageError}>{errors.name?.message}</span>
+            <Input text={"Email"} placeholder="Informe seu e-mail" type='email' innerRef={register("email")}/>
+            <span className={s.messageError}>{errors.email?.message}</span>
+            <Input text={"Password"} placeholder="Informe seu e-mail" type="password" innerRef={register("password")}/>
+            <span className={s.messageError}>{errors.password?.message}</span>
+            <Input text={"Confirme sua senha"} placeholder="Confirme sua senha" type="password" innerRef={register("confirmPassword")}/>
+            <span className={s.messageError}>{errors.confirmPassword?.message}</span>
             <Button textInfo={'Cadastrar-se'} />
         </form>
     )
